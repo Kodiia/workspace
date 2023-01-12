8465edb3ed5de94b4b2c0bec76d5ae3ab1e74e1f
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2023-01-12 16:14:53 +0000

    initial commit

--- src/db/db.js
+++
@@ -1,3 +0,0 @@
-import PocketBase from 'pocketbase';
-
-export const pb = new PocketBase('https://kodiia-db.pockethost.io');
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,29 +1,5 @@
-import { pb } from "../db/db"
+//import { pb } from "../db/db"
 import { serializeNonPOJOs } from '$lib/utils';
-
-// export async function load(){
-//     // return{
-//     //     files: await locals.pb.
-//     // }
-
-//     const getProjects = async () =>{
-
-//         try{
-//             const result = await pb.collection('test_projects').getList(1, 20, { '$autoCancel': false }
-//             );
-//             console.log(result)
-//             return serializeNonPOJOs(result)
-//         } catch (err){
-//             console.log(err)
-//             //throw error(err.status, err.message);
-//         }
-//     }
-
-//     return {
-//         result: getProjects()
-//     }
-// }
-
 
 export async function load({locals}){
 
