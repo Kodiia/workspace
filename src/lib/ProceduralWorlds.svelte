998cbc9fe4a3704f46fdae72df3c7c2430890690
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2023-08-18 14:21:23 +0000

    procedural world generator

--- src/lib/ProceduralWorlds.svelte
+++ src/lib/ProceduralWorlds.svelte
@@ -2,12 +2,166 @@
     import { T } from '@threlte/core'
     import { OrbitControls, interactivity, InstancedMesh, Instance } from '@threlte/extras'
 	import { scale } from 'svelte/transition';
+	import { HemisphereLight } from 'three';
 	//import { InstancedMesh, PerspectiveCamera } from 'three';
 
     let thescale = 1.0
     let color = 'white'
     interactivity()
+
+
+    let points = []
+    let cells = []
+    const widthNum = 10
+    const depthNum = 10
+    const heightNum = 10
+
+    function generateCells(widthNum, depthNum, heightNum){
+    for(let i=0; i<widthNum; i++){
+        cells[i] = []
+        for(let j=0; j<depthNum; j++){
+            cells[i][j] = []
+            for(let k=0; k<heightNum; k++){
+                if(Math.random()>0.5){
+                    cells[i][j][k] = {
+                        aliveNow: true,
+                        aliveNext: false,
+                        liveNeighbors: 0
+                    }
+                } else {
+                    cells[i][j][k] = {
+                        aliveNow: false,
+                        aliveNext: false,
+                        liveNeighbors: 0
+                    }
+                }
+            }
+        
+        // if(Math.random() > 0.5){
+        //   cells[i][j] = {
+        //     aliveNow: true,
+        //     aliveNext: false,
+        //     liveNeighbours: 0
+        //   }
+        // } else {
+        //   cells[i][j] = {
+        //     aliveNow: false,
+        //     aliveNext: false,
+        //     liveNeighbours: 0
+        //   }
+        // }
+        
+        // if(i == 0 && j == 0 || i === width-1 && j === height-1 || i === 0 && j === height-1 || i === width-1 && j === 0){
+        //     cells[i][j] = {
+        //     aliveNow: true,
+        //     aliveNext: false,
+        //     liveNeighbours: 0
+        //     }
+        // } else {
+        //     cells[i][j] = {
+        //     aliveNow: false,
+        //     aliveNext: false,
+        //     liveNeighbours: 0
+        //     }
+        // }
+
+        }
+    }
+    }
+
+
+    function cellIsAliveNextGeneration(){
+    for(let i=0; i<cells.length; i++){
+        for(let j=0; j<cells[i].length; j++){
+            for(let k=0; k<cells[i][j].length; k++){
+            cells[i][j][k].liveNeighbours = 0
+
+                if(typeof cells[i-1] != 'undefined' && cells[i-1][j][k].aliveNow){
+                cells[i][j][k].liveNeighbours++
+                }
+                if(typeof cells[i+1] != 'undefined' && cells[i+1][j][k].aliveNow){
+                cells[i][j][k].liveNeighbours++
+                }
+                if(typeof cells[i][j-1] != 'undefined' && cells[i][j-1][k].aliveNow){
+                cells[i][j][k].liveNeighbours++
+                }
+                if(typeof cells[i][j+1] != 'undefined' && cells[i][j+1][k].aliveNow){
+                cells[i][j][k].liveNeighbours++
+                }
+                if(typeof cells[i][j][k-1] != 'undefined' && cells[i][j][k-1].aliveNow){
+                cells[i][j][k].liveNeighbours++
+                }
+                if(typeof cells[i][j][k+1] != 'undefined' && cells[i][j][k+1].aliveNow){
+                cells[i][j][k].liveNeighbours++
+                }
+
+            if(cells[i][j][k].liveNeighbours <= 2 || cells[i][j][k].liveNeighbours >= 5){
+                cells[i][j][k].aliveNext = false
+            } else {
+                cells[i][j][k].aliveNext = true
+            }
+            
+            }
+    }
+    }
+    }
+
+    function resetGeneration(){
+        for(let i=0; i<cells.length; i++){
+            for(let j=0; j<cells[i].length; j++){
+                for(let k=0; k<cells[i][j].length; k++){  
+                    cells[i][j][k].aliveNow = cells[i][j][k].aliveNext
+                }
+            }
+    }
+    }
+
+
+    function getPointsCoords(){
+    for(let i=0; i<cells.length; i++){
+        for(let j=0; j<cells[i].length; j++){
+            for(let k=0; k<cells[i][j].length; k++){  
+        
+            if(cells[i][j][k].aliveNow){          
+                points.push({
+                x: i,
+                y: k,
+                z: j,
+                color: 'white',
+                scale: 1
+                })
+            }
+            }
+        }
+    }
+    }
+
+    function updateWorld(){
+        cells = []
+        generateCells(widthNum, depthNum, heightNum)
+
+        for(let i=0; i<10; i++){
+            cellIsAliveNextGeneration()
+            resetGeneration()
+        }
+
+        getPointsCoords()
+
+        // console.log(scene)
+    }
+
+    updateWorld()
+
+    function removeElementfromPoints(x = 0, y = 0, z = 0){
+        for(let i=0; i<points.length; i++){
+            if(points[i].x === x && points[i].y === y && points[i].z === z){
+                points.splice(i, 1)
+            }
+        }
+    }
 </script>
+
+
 
 <T.PerspectiveCamera
     makeDefault
@@ -19,19 +173,26 @@
 <OrbitControls />
 </T.PerspectiveCamera>
 
-<T.Mesh >
+<T.HemisphereLight args={['white', 'skyblue']}/>
+
+<!-- <T.Mesh >
    <T.BoxGeometry args={[1, 1, 1]} />
    <T.MeshBasicMaterial color="red" />
-</T.Mesh>
+</T.Mesh> -->
 
 <InstancedMesh >
-    <T.SphereGeometry args={[0.5]} />
-    <T.MeshBasicMaterial color={color} />
+    <T.BoxGeometry args={[1, 1, 1]} />
+    <T.MeshStandardMaterial color={'hotpink'} />
+
+    {#each points as {x, y, z, color, scale}}
     <Instance
-        position.x={-2}
-        position.y={Math.sin(2)}
-        scale={thescale}
-        on:pointerenter={() => {thescale = 1.5; color = 'blue'}}
-        on:pointerleave={() => {thescale = 1.0; color = 'white'}}
+        position.x={x}
+        position.y={y}
+        position.z={z}
+        scale={scale}
+        on:pointerdown={(e) => {removeElementfromPoints(x, y, z); console.log(e.normal, e.object.position)}}
+        on:pointerenter={() => {scale = 0.85}}
+        on:pointerleave={() => {scale = 1.0}}
     />
+    {/each}
 </InstancedMesh>
--- src/routes/games/procedural-worlds/+page.svelte
+++ src/routes/games/procedural-worlds/+page.svelte
@@ -2,15 +2,113 @@
     import { Canvas } from '@threlte/core'
     import ProceduralWorlds from '$lib/ProceduralWorlds.svelte';
     import { width, height } from '$lib/store'
+    import { fade } from 'svelte/transition';
+
+    let navMenuDisplay = 'none'
+
+
 </script>
 
+<nav>
+    <div class='navButtons'>
+        <button class='menuButton' on:click={()=>{navMenuDisplay === 'none' ? navMenuDisplay = 'block' : navMenuDisplay = 'none'}}>
+            {#if navMenuDisplay === 'none'}
+                <svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+                    <line x1="10" y1="20" x2="40" y2="20" stroke="#1a1a1a" stroke-width='2' />
+                    <line x1="10" y1="30" x2="40" y2="30" stroke="#1a1a1a" stroke-width='2' />
+                </svg>
+            {:else}
+                <svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+                    <line x1="15" y1="15" x2="35" y2="35" stroke="#1a1a1a" stroke-width='2' />
+                    <line x1="15" y1="35" x2="35" y2="15" stroke="#1a1a1a" stroke-width='2' />
+                </svg>
+            {/if}
+        </button>
+        <!-- <img src='{habitat}' width='100' alt='habitat'/> -->
+        <a class='smallMenuButton' href='/'>Home</a>
+    </div>
+    <hr style='display: {navMenuDisplay}'>
+    <div class='navMenu' style='display: {navMenuDisplay}; max-height: calc({$height}px - 70px);' >
+        
+        <div class='statisticsContainer' style='margin-top: 10px;'>
+            <h2>Some setups</h2>
+        </div>
+        <div class='challengesContainer'>
+        </div>
+    </div>
+</nav>
+
+
 <div class='container' style='width: {$width}px; height: {$height}px;'>
-<Canvas>
-    <ProceduralWorlds />
-</Canvas>
+    <Canvas>
+        <ProceduralWorlds />
+    </Canvas>
 </div>
 
 <style>
+    nav{
+        width: calc(100% - 10px);
+        min-height: 50px;
+        /* display: flex; */
+        /* margin-bottom: 10px; */
+        position: fixed;
+        padding: 5px;
+        top: 0;
+        left: 0;
+        background: linear-gradient( 45deg, #ffffffc3, #ffffff90);
+        backdrop-filter: blur(40px);
+        -webkit-backdrop-filter: blur(40px);
+        border-radius: 0 0 10px 10px;
+        z-index: 10;
+    }
+    .navButtons{
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+    }
+    .navMenu{
+        /* width: calc(100% - 10px); */
+        overflow-y: scroll;
+        margin-bottom: 10px;
+    }
+    .smallMenuButton {
+      background: none;
+      border: none;
+      color: #1a1a1a;
+      font-family: Roboto, sans-serif;
+      font-weight: 300;
+      font-size: 1rem;
+      text-decoration: none;
+      display: flex;
+      align-items: center;
+      padding: 0 20px 0 0;
+    }
+    .smallMenuButton:hover {
+      background: none;
+      border: none;
+      font-family: Roboto, sans-serif;
+      font-weight: 300;
+      font-size: 1rem;
+      color: #3d95ee;
+      text-decoration: underline;
+      display: flex;
+      align-items: center;
+    }
+    .menuButton, .assetsMenuButton{
+        padding: 5px;
+        width: 50px;
+        height: 50px;
+        border: none;
+        transform: scale(1.0);
+    }
+    .menuButton:hover, .assetsMenuButton:hover{
+        background: none;
+        transform: scale(1.1);
+    }
+    .statisticsContainer, .challengesContainer{
+        /* width: 100%; */
+        padding: 10px;
+    }
     .container{
         background: pink;
     }
