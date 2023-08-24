7bad5c38bb8110bce896d656861e94d82ed3e6ec
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2023-08-24 14:45:42 +0000

    register update

--- src/routes/register/+page.server.js
+++ src/routes/register/+page.server.js
@@ -3,9 +3,10 @@ import { error, redirect } from '@sveltejs/kit'
 export const actions = {
     register: async ({ locals, request }) => {
         const body = Object.fromEntries(await request.formData())
+        const specialCourses = '{ "specialCourses" : [] }' 
 
         try {
-            await locals.pb.collection('users').create({...body})
+            await locals.pb.collection('users').create({specialCourses, ...body})
             await locals.pb.collection('users').requestVerification(body.email)
         } catch (err) {
             console.log('Error: ', err)
