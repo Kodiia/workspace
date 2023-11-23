dc14674214f166111c382145f1da959152b5ada0
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2023-11-23 19:08:46 +0000

    api test

--- src/routes/api/images/[collectionName]/[recordId]/[fileName]/+server.js
+++ src/routes/api/images/[collectionName]/[recordId]/[fileName]/+server.js
@@ -2,7 +2,7 @@ import { error } from '@sveltejs/kit';
 import { getImageUrl } from '$lib/utils'
 
 export async function GET({locals, params}){
-    const url = locals.pb.baseUrl + getImageUrl(params.collectionName, params.recordId, params.fileName)
+    const url = locals.pb.baseUrl + '/' + getImageUrl(params.collectionName, params.recordId, params.fileName)
 
     const response = await fetch(url)
     const blob = await response.blob()
