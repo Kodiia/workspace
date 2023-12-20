1b106b14ecf0117d473ea615bfea9e1945f34604
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2023-12-20 15:22:20 +0000

    save test

--- src/routes/games/procedural-worlds/[projectId]/+page.svelte
+++ src/routes/games/procedural-worlds/[projectId]/+page.svelte
@@ -212,6 +212,7 @@
         <button id='createButton' on:click={()=>{proceduralWorld.getAssetsData(x, z, y)}} style='margin: 20px 0 0 10px;'>Create World</button>
         <form method='POST' action='?/updateProject' enctype='multipart/form-data'>
            <textarea name='assetsDataJSON' bind:this={textAreaValue} value='' style='display: none'></textarea>
+           <textarea name='assetsData' value='{$worldData.assets}' style='display: none'></textarea>
            <button formaction="?/updateProject" style='margin-top: 20px; margin-right: 15px;' on:click={()=>{setTextAreaValue($worldData.assets); console.log(JSON.stringify($worldData.assets))}}>Save World</button>
         </form>
     </div>
