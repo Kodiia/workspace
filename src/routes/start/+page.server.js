369f5a07dbadd62836c94019d7d8862523ac5ba0
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2024-02-25 13:29:48 +0000

    create file test

--- src/routes/start/+page.server.js
+++ src/routes/start/+page.server.js
@@ -4,16 +4,23 @@ export const actions = {
     createProject: async ({ locals, request }) => {
         const body = Object.fromEntries(await request.formData())
 
-        const projectData = {
-            name: body.name,
-            files: {
-              'index.html': '<html><head><title>Your Project</title></head><body><h1>Welcome to Your Project</h1></body></html>'
-            }
-          };
+        const formData = new FormData();
+        formData.append('name', body.name);
+
+        const file = new File(["<html><head><title>Your Project</title></head><body><h1>Welcome to Your Project</h1></body></html>"], "index.html", {
+            type: "text/plain",
+          });
+        formData.append('files', file);
+
+        // const projectData = {
+        //     name: body.name,
+        //     files: {
+        //       'index.html': '<html><head><title>Your Project</title></head><body><h1>Welcome to Your Project</h1></body></html>'
+        //     }
+        //   };
 
         try {
-            await locals.pb.collection('userProjects').create(projectData)
-            // await locals.pb.collection('users').requestVerification(body.email)
+            await locals.pb.collection('userProjects').create(formData)
         } catch (err) {
             console.log('Error: ', err)
             throw error(500, 'Something went wrong')
