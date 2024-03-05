42270e6d063d76360b25d9a758630841cc98315d
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2024-03-05 07:29:49 +0000

    delete project test

--- src/routes/api/projects/[projectId]/delete/+server.js
+++ src/routes/api/projects/[projectId]/delete/+server.js
@@ -2,13 +2,17 @@ import { error, redirect } from '@sveltejs/kit';
 
 export async function DELETE({locals, params}){
     const projectId = params.projectId
-    // const project = await locals.pb.collection('userProjects').getOne(`${projectId}`)
+    const project = await locals.pb.collection('userProjects').getOne(`${projectId}`)
+    console.log(projectId, project)
 
     if(locals.user){
         try {
-            await locals.pb.collection('userProjects').delete(`"${projectId}"`)
+            const result = await locals.pb.collection('userProjects').delete(`"${projectId}"`)
 
-            throw redirect( 303, '/projects')
+            // throw redirect( 303, '/projects')
+
+            return new Response(JSON.stringify(result), {status: 200})
+                
         } catch (err){
             console.log(err)
             throw error(err.status, err.message);
@@ -17,5 +21,4 @@ export async function DELETE({locals, params}){
         throw redirect( 303, '/')
     }
 
-    
 }
