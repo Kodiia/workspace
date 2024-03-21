8dd5f029f847a886686618ecf697ae50ffdb56d1
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2024-03-21 18:20:54 +0000

    verification test

--- src/routes/verify/[token]/+server.js
+++ src/routes/verify/[token]/+server.js
@@ -3,10 +3,11 @@ import { error, redirect } from '@sveltejs/kit'
 export const GET = async ({ locals, params }) => {
     try {
         await locals.pb.collection('users').confirmVerification(params.token);  
-        throw redirect(303, '/login')
+        // return { success: true }
     } catch (err) {
         console.log('Error: ', err)
         throw error(500, 'Something went wrong while verification process')
     }
     
+    throw redirect(303, '/login')
 }
