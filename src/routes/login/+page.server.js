68d781d33cfa9f04d6d3b2faeaae645f5f822a1b
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2024-03-23 13:49:34 +0000

    last login test

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -14,17 +14,15 @@ export const actions = {
                 // Update the last login time for the authenticated user
                 await locals.pb.collection("users").update(locals.pb.authStore.model.id, { lastLogin: currentTime });
                 console.log(locals.pb.authStore.model.id, currentTime);
-            }
 
-            if(!locals.pb?.authStore?.model?.verified){
+                // Redirect or perform other actions based on the authentication state
+                throw redirect(303, '/');
+            }else{
                 locals.pb.authStore.clear()
                 return {
                     notVerified: true
                 }
             }
-
-            // Redirect or perform other actions based on the authentication state
-            throw redirect(303, '/');
         } catch (err) {
             console.log('Error: ', err)
             throw error(500, 'Something went wrong while logging in')
