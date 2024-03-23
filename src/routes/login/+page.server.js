5b018ab3f5f2407951a7749f509d591795a68486
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2024-03-23 13:36:15 +0000

    last login test

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -5,14 +5,24 @@ export const actions = {
         const body = Object.fromEntries(await request.formData())
 
         try {
-            const user = await locals.pb.collection('users').authWithPassword(body.email, body.password)
-            const currentTime = new Date().toISOString();
-            await locals.pb.collection("users").update(user.id, { lastLogin: currentTime });
-            console.log(user.id, currentTime)
-            // if (user) {
-            //     const currentTime = new Date().toISOString();
-            //     await locals.pb.collection("users").update(user.id, { lastLogin: currentTime });
-            //  }
+            // const user = await locals.pb.collection('users').authWithPassword(body.email, body.password)
+            // const currentTime = new Date().toISOString();
+            // await locals.pb.collection("users").update(user.id, { lastLogin: currentTime });
+            // console.log(user.id, currentTime)
+
+            // Authenticate the user
+            await locals.pb.collection('users').authWithPassword(body.email, body.password);
+
+            // Check if the authentication was successful
+            if (locals.pb.authStore.isValid) {
+                const currentTime = new Date().toISOString();
+                // Update the last login time for the authenticated user
+                await locals.pb.collection("users").update(locals.pb.authStore.model.id, { lastLogin: currentTime });
+                console.log(locals.pb.authStore.model.id, currentTime);
+
+                // Redirect or perform other actions based on the authentication state
+                throw redirect(303, '/');
+            }
 
             if(!locals.pb?.authStore?.model?.verified){
                 locals.pb.authStore.clear()
