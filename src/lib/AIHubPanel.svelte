7c343fb6b63abc84037d67a1ad2dafbf3cbc67cd
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2024-06-18 20:39:32 +0000

    ai api url

--- src/lib/AIHubPanel.svelte
+++ src/lib/AIHubPanel.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { onMount } from 'svelte';
-	import AI_API_URL from '$env/static/private'
+	import { PUBLIC_AI_API_URL } from '$env/static/public'
 	import { getFileLogoURL } from '$lib/utils';
 	import {
 		bgColor,
@@ -57,7 +57,7 @@
 		try {
 			isFetchingRequest = true;
 			// const answerData = await fetch(`/api/transformers/rag/${query}`);
-			const answerData = await fetch(`${AI_API_URL}/${query}`);
+			const answerData = await fetch(`${PUBLIC_AI_API_URL}/${query}`);
 			const answerDataObject = await answerData.json();
 			modelAnswer = answerDataObject;
 			answers = [...answers, modelAnswer];
