023822a7d4f9324ff9276253461afee7a87cc5b3
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2024-12-08 13:02:54 +0000

    templates update

---
+++ src/routes/demo/+page.svelte
@@ -0,0 +1,11 @@
+<iframe src='https://diffusion-workflow-production.up.railway.app' 
+title='demo'
+allow="accelerometer; camera; encrypted-media; display-capture; geolocation; gyroscope; microphone; midi; clipboard-read; clipboard-write; web-share" sandbox="allow-forms allow-modals allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation allow-downloads allow-presentation" name="Kodiia workspace"
+></iframe>
+<style>
+    iframe{
+        width: 100%;
+        height: 100vh;
+        border: none;
+    }
+</style>
